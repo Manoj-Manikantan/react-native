10121b079d45b8de594c938ab2d196c6aeede37f
Manoj Manikantan <manojmkt127@gmail.com>
2020-09-25 23:21:08 -0400

    initiated lab1

---
+++ lab1/app.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My details</Text>
+      <Text>Manoj Manikantan Muralidharan</Text>
+      <Text>301067347</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
