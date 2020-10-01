f2bd3490a9c3409eeb698b0a6412217f73546f5b
Manoj <manoj@Manojs-iMac.local>
2020-10-01 13:40:23 -0400

    feat--category added and styles updated

--- assignments/assignment1/App.js
+++ assignments/assignment1/App.js
@@ -1,59 +1,93 @@
-import React, {useState} from 'react';
-import { Button, Picker, StyleSheet, Text, TextInput, TouchableOpacity, View, Alert} from 'react-native';
+import React, { useState } from 'react';
+import { Picker, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
 
-  const[dropdownValue, setDropdownValue] = useState('SI')
-  const[heightValue, setHeightValue] = useState('');
-  const[weightValue, setWeightValue] = useState('');
-  const[bmiValue, setBmiValue] = useState(0);
+  const [dropdownValue, setDropdownValue] = useState('SI')
+  const [heightValue, setHeightValue] = useState('');
+  const [weightValue, setWeightValue] = useState('');
+  const [bmiValue, setBmiValue] = useState(0);
+  const [bmiResult, setBmiResult] = useState('');
 
   const calculateBMI = (height, weight) => {
-    if(isNaN(height) || isNaN(weight))
-    {
-      console.log('Wrong input');
+    if (isNaN(height) || isNaN(weight)) {
       alert('Incorrect input');
+      clearValues()
     }
-    else
-    {
-      var bmiVal = (parseFloat(weight)*10000)/(parseFloat(height)*parseFloat(height));
+    else {
+      var bmiVal = 0
+      var bmiCategory = ''
+      if (dropdownValue == "SI") {
+        bmiVal = (parseFloat(weight) / (parseFloat(height) * parseFloat(height)) * 10000);
+      } else {
+        bmiVal = (parseFloat(weight) / parseFloat(height) / parseFloat(height)) * 703;
+      }
       bmiVal = bmiVal.toFixed(2);
+      if (bmiVal < 18.5) {
+        bmiCategory = 'Underweight'
+      } else if ((bmiVal >= 18.5) && (bmiVal < 25)) {
+        bmiCategory = 'Normal Weight'
+      } else if ((bmiVal >= 25) && (bmiVal < 30)) {
+        bmiCategory = 'Overweight'
+      } else if (bmiVal >= 30) {
+        bmiCategory = 'Obese'
+      }
       setBmiValue(bmiVal)
+      setBmiResult(bmiCategory)
     }
+  }
+
+  const clearValues = () => {
     setHeightValue('')
     setWeightValue('')
+    setBmiValue(0)
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>BMI Calculator</Text>
+      <Text style={styles.title}>BMI Calculator</Text><br />
       <Text>Measurement System</Text>
       <Picker
+        style={styles.centreTextAlign}
         selectedValue={dropdownValue}
         onValueChange={(itemValue, itemIndex) => setDropdownValue(itemValue)}
       >
         <Picker.Item label="SI [ Cms, Kgs ]" value="SI" />
         <Picker.Item label="Imperial [ Inches, Lbs ]" value="Imperial" />
-      </Picker>
+      </Picker><br /><br />
       <Text>Height</Text>
       <TextInput style={styles.inputBox}
-	      onChangeText={text => setHeightValue(text)}
+        placeholder={dropdownValue == "SI" ? "Cms" : "Inches"}
+        onChangeText={text => setHeightValue(text)}
         clearTextOnFocus="true"
         value={heightValue}
       />
       <Text>Weight</Text>
       <TextInput style={styles.inputBox}
-	      onChangeText={text => setWeightValue(text)}
+        placeholder={dropdownValue == "SI" ? "Kgs" : "Lbs"}
+        onChangeText={text => setWeightValue(text)}
         clearTextOnFocus="true"
         value={weightValue}
       />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => calculateBMI(heightValue, weightValue)}
-      >
-      <Text>Calculate</Text>
-      </TouchableOpacity>
-      <Text>{bmiValue > 0 ? "Your BMI is " + bmiValue : " "}</Text>
+      <View style={{ flexDirection: "row" }}>
+        <View style={{ flex: 1 }}>
+          <TouchableOpacity
+            style={styles.buttonCalculate}
+            onPress={() => calculateBMI(heightValue, weightValue)}
+          >
+            <Text style={styles.calculateText}>Calculate</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flex: 1 }}>
+          <TouchableOpacity
+            style={styles.buttonClear}
+            onPress={() => clearValues()}
+          >
+            <Text style={styles.centreTextAlign}>Clear</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Text style={styles.centreTextAlign}>{bmiValue > 0 ? " Results! \n " + "Your BMI : " + bmiValue + "\n Your BMI Category : " + bmiResult : " "}</Text>
     </View>
   );
 }
@@ -61,7 +95,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -70,15 +104,36 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
   inputBox: {
+    backgroundColor: 'white',
     borderColor: 'gray',
     borderWidth: 1,
     height: 20
   },
-  button: {
-    backgroundColor: 'gray',
+  buttonCalculate: {
+    backgroundColor: 'darkgreen',
     borderWidth: 1,
     padding: 5,
     marginTop: 20,
-    marginBottom: 20
+    marginBottom: 20,
+    justifyContent: "flex-start",
+    width: 100,
+    marginRight: 10
+  },
+  calculateText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  buttonClear: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    padding: 5,
+    marginTop: 20,
+    marginBottom: 20,
+    justifyContent: "flex-end",
+    width: 100,
+    marginLeft: 10
+  },
+  centreTextAlign: {
+    textAlign: 'center'
   }
 });
